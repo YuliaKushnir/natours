a48956419b668761772cd4b9f087aa7ace791f96
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-03-15 08:31:06 +0200

    Added a comment

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const cookieParser = require('cookie-parser');
 
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
