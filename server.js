b68becae36bd81022678affe745494be626e7f53
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-06-03 20:44:40 +0300

    Changed port for rendering service

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ mongoose
 }).catch(err => console.log('ERRORR'));
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+
+const server = app.listen(port, '0.0.0.0', () => {
   console.log(`App running on port ${port}...`);
 });
 
