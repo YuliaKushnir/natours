3cff2d81bfffeb89d8d82bfe640fc8d7b04b2b5c
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-06-04 10:02:35 +0300

    Deleted default port

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, '0.0.0.0', () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
